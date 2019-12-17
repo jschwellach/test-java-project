f6af17336e0aa21d79e7ae83290dd915226d63a0
Janos Schwellach <jschwellach@gmail.com>
2019-12-17 16:27:02 +0800

    main class

---
+++ src/main/java/de/schwellach/examples/Main.java
@@ -0,0 +1,18 @@
+package de.schwellach.examples;
+
+public class Main {
+	
+	public Main() {
+		init();
+	}
+	
+	protected void init() {
+		
+	}
+	
+
+	public static void main(String[] args) {
+		new Main();
+	}
+
+}
